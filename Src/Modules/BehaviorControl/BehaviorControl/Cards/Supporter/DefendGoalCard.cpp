2912ef646126bd767dc613679e2dd2625a1442e1
jose <josegzm08@gmail.com>
2024-01-10 18:29:54 -0500

    try to fix a bug

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/Supporter/DefendGoalCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/Supporter/DefendGoalCard.cpp
@@ -1,57 +1,34 @@
-#include "Representations/BehaviorControl/FieldBall.h"
 #include "Representations/BehaviorControl/Skills.h"
-#include "Representations/Configuration/FieldDimensions.h"
-#include "Representations/Modeling/RobotPose.h"
 #include "Tools/BehaviorControl/Framework/Card/Card.h"
 
-CARD(DefendGoalCard,{,
-    CALLS(Activity),//los llamados son para llamar todas las skills se hace por CALLS()
+CARD(DefendGoalCard,
+{,
+  CALLS(Activity),
+  CALLS(LookForward),
+  CALLS(Say),
+  CALLS(Stand),
+});
 
-    REQUIRES(FieldBall),//Llama a los representaciones que necesita, siempre se usan estas por lo general
-    REQUIRES(FieldDimensions),
-    REQUIRES(RobotPose),
-    DEFINES_PARAMETERS( 
-    //Aca son todas las variables que se quiera definir para despues usarse dentro de la clase
-    {,//Mirar cuales sirven y cuales no
-        //definir parametros
-    }),
-}); 
-
-class DefendGoalCard : public DefendGoalCard
+class DefendGoalCard : public CodeReleasePositionForKickOffCardBase
 {
-    bool preconditions() const override
-    {
-        return true;
-    }
+  bool preconditions() const override
+  {
+    return true;
+  }
 
-    bool postconditions() const override
-    {
-        return true;
-    }
+  bool postconditions() const override
+  {
+    return true;
+  }
 
-    option{
-      theActivitySkill(BehaviorStatus::DefendGoalCard);
-      initial_state(start){
-        transition{
-          if(state_time > initialWaitTime)
-            //Aca es el cambio de escena
-        }
-        action{
-          
-          //Lo que quieres que haga cuando la se llegue a este estado
-        }
-      }
-      state(){
-        transition{
-            if()//Condicion para que pase al siguiente bloque
-                //siguiente bloque
-        }
-        action{
-        
-        }
-      }
-      //CRear los estados que sean necesarios, debe ser ciclico en la mayoria de casos.
-    }
-}
-//Esto es lo que crea la carta
-MAKE_CARD(DefendGoalCard)
+  void execute() override
+  {
+    theActivitySkill(BehaviorStatus::codeReleasePositionForKickOff);
+    theLookForwardSkill();
+    theStandSkill();
+    // Not implemented in the Code Release.
+    theSaySkill("Please implement a behavior for me!");
+  }
+};
+
+MAKE_CARD(DefendGoalCard);
