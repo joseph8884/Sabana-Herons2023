deb3a64fe29a4305dee2ae713ca7668462e834d0
jose <josegzm08@gmail.com>
2024-02-09 18:57:53 -0500

    proximidadCard worke now

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/ProximidadBalon.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/ProximidadBalon.cpp
@@ -1,34 +1,59 @@
+#include "Representations/BehaviorControl/FieldBall.h"
 #include "Representations/BehaviorControl/Skills.h"
+#include "Representations/Configuration/FieldDimensions.h"
+#include "Representations/Modeling/RobotPose.h"
 #include "Tools/BehaviorControl/Framework/Card/Card.h"
+#include "Tools/BehaviorControl/Framework/Card/CabslCard.h"
 
-CARD(ProximidadBalonCard,
-{,
-  CALLS(Activity),
-  CALLS(LookForward),
-  CALLS(Say),
-  CALLS(Stand),
-});
+CARD(ProximidadBalonCard,{,
+    CALLS(Activity),//los llamados son para llamar todas las skills se hace por CALLS()
+    CALLS(LookForward),
+    REQUIRES(FieldBall),//Llama a los representaciones que necesita, siempre se usan estas por lo general
+    REQUIRES(FieldDimensions),
+    REQUIRES(RobotPose),
+    DEFINES_PARAMETERS( 
+    //Aca son todas las variables que se quiera definir para despues usarse dentro de la clase
+    {,//Mirar cuales sirven y cuales no
+        //definir parametros
+        (int)(1000) initialWaitTime,
+    }),
+}); 
 
-class ProximidadBalonCard : public DefendGoalCardBase
+class ProximidadBalonCard : public ReturnToGoalCardBase
 {
-  bool preconditions() const override
-  {
-    return true;
-  }
+    bool preconditions() const override
+    {
+        return true;
+    }
 
-  bool postconditions() const override
-  {
-    return true;
-  }
+    bool postconditions() const override
+    {
+        return true;
+    }
 
-  void execute() override
-  {
-    theActivitySkill(BehaviorStatus::codeReleasePositionForKickOff);
-    theLookForwardSkill();
-    theStandSkill();
-    // Not implemented in the Code Release.
-    theSaySkill("Please implement a behavior for me!");
-  }
+    option{
+      theActivitySkill(BehaviorStatus::unknown);
+      initial_state(start){
+        transition{
+          if(state_time > initialWaitTime)
+            goto siguienteBloque;//Aca es el cambio de escena
+        }
+        action{
+          theLookForwardSkill();
+          //Lo que quieres que haga cuando la se llegue a este estado
+        }
+      }
+      state(siguienteBloque){
+        transition{
+            if(true)//Condicion para que pase al siguiente bloque
+              goto start;  //siguiente bloque
+        }
+        action{
+          theLookForwardSkill();
+        }
+      }
+      //CRear los estados que sean necesarios, debe ser ciclico en la mayoria de casos.
+    }
 };
-
-MAKE_CARD(ProximidadBalonCard);
+//Esto es lo que crea la carta
+MAKE_CARD(ProximidadBalonCard)
